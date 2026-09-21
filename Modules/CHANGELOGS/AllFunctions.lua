72d5a0440adf52928ad78a9bf03ade06d1fc9b07
milipenya <milipenya@gmail.com>
2026-09-21 14:48:54 +0300

    Update AllFunctions.lua

--- Modules/CHANGELOGS/AllFunctions.lua
+++ Modules/CHANGELOGS/AllFunctions.lua
@@ -1,1 +1,49 @@
+--[[
+    PenyaHubZ
+    Modules/ChangeLogs/AllFunctions.lua
 
+    Модуль CHANGE LOGS.
+]]
+
+local ChangeLogs = {}
+
+ChangeLogs.Name = "CHANGE LOGS"
+ChangeLogs.Version = "0.1.0"
+
+--------------------------------------------------
+-- Init
+--------------------------------------------------
+
+function ChangeLogs:Init(context)
+    self.Context = context
+
+    print("[PenyaHubZ] ChangeLogs initialized.")
+
+    return true
+end
+
+--------------------------------------------------
+-- Information
+--------------------------------------------------
+
+function ChangeLogs:GetVersion()
+    return self.Version
+end
+
+function ChangeLogs:GetName()
+    return self.Name
+end
+
+--------------------------------------------------
+-- Destroy
+--------------------------------------------------
+
+function ChangeLogs:Destroy()
+    self.Context = nil
+
+    print("[PenyaHubZ] ChangeLogs destroyed.")
+
+    return true
+end
+
+return ChangeLogs
